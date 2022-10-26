99e48e1c7dc77f6487f86864313ecf7c2af928e5
Dineth_112221 <manuja.gayan@axiatadigitallabs.com>
2022-10-26 09:07:16 +0530

    queue activity

--- app/src/main/java/edu/sliitead/fillfuelapp/QueueActivity.java
+++ app/src/main/java/edu/sliitead/fillfuelapp/QueueActivity.java
@@ -1,18 +1,23 @@
 package edu.sliitead.fillfuelapp;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class QueueActivity extends AppCompatActivity {
 
     TextView txtCompanyBack;
+    Button btnQueueExit;
+    AlertDialog.Builder alertDialogBuilder;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -24,6 +29,9 @@ public class QueueActivity extends AppCompatActivity {
         setContentView(R.layout.activity_queue);
 
         txtCompanyBack = (TextView) findViewById(R.id.txtCompanyBack);
+        btnQueueExit = (Button) findViewById(R.id.btnQueueExit);
+
+        alertDialogBuilder = new AlertDialog.Builder(this);
 
         txtCompanyBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +39,30 @@ public class QueueActivity extends AppCompatActivity {
                 showSearch();
             }
         });
+
+        btnQueueExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialogBuilder.setTitle("Exit confimation from fuel queue.")
+                        .setMessage("Are you sure to exit from the queue?")
+                        .setCancelable(true)
+                        .setPositiveButton("Exit without fuel pump", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("Exit after fuel pump", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();
+                            }
+                        })
+                        .show();
+
+            }
+        });
+
     }
 
     private void showSearch() {
